7801d85ff2f5b7db23c15b95571059607ad8ec6b
Mc-Zen <52877387+Mc-Zen@users.noreply.github.com>
2025-04-19 13:44:40 +0200

    [move] PreviewedCode component into folder

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
-import PreviewedCode from '@site/src/components/PreviewedCode.js';
+import PreviewedCode from '@site/src/components/PreviewedCode';
 import { Parameters, Param, ParamName, Signature, Default, SignatureName, SignatureParam } from '@site/src/components/Docs/FunctionDocumentation.js';
 import { Crossref } from '@site/src/components/Docs/Crossref.js';
 import { ExampleCards } from '@site/src/components/Docs/ExampleCards.js';
