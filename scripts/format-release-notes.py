3161948e3a92a74ed1228f465d4709b308e74803
Mc-Zen <52877387+Mc-Zen@users.noreply.github.com>
2025-11-03 12:20:23 +0100

    [fix] regex in release note formatting script

--- scripts/format-release-notes.py
+++ scripts/format-release-notes.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     with open(f"blog/release-{version}.md", "r", encoding="utf-8") as file:
         content = file.read()
     
-    content = re.sub("<Crossref target=\"([\\w#\\.\\-]*)\" />", "[\\1](https://lilaq.org/docs/reference/\\1)", content)
-    content = re.sub("\\[(\\w*)#(\\w*)\\]", "[\\1.\\2]", content)
+    content = re.sub("<Crossref target=\"([\\w#\\.\\-]*)\" />", "[`\\1`](https://lilaq.org/docs/reference/\\1)", content)
+    content = re.sub("\\[`(\\w*)#(\\w*)`\\]", "[`\\1.\\2`]", content)
     content = content.replace("](/", "](https://lilaq.org/")
     content = content.replace("<!-- truncate -->", "")
     assert "Crossref" not in content, "Something with the link converison failed"
