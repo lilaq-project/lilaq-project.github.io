3fbeaec657d78e0dedab38dd80826c8063fc819f
Mc-Zen <52877387+Mc-Zen@users.noreply.github.com>
2025-12-19 22:26:50 +0100

    [remove] debug print statement

--- src/components/Docs/Typstref.js
+++ src/components/Docs/Typstref.js
@@ -3,7 +3,6 @@ import styles from './docs.module.css';
 // Reference to a built-in definition of Typst
 export const Typstref = ({ children, target }) => {
 
-    console.log(target)
     // const display = target.split("/").slice(-1)
     return (
         <a href={target} className={styles.typstRef} target="_blank">
